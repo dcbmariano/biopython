29212e1c0aca78b5818204e7c1f63e9a9eab084d
dcbmariano <diogohenks@hotmail.com>
2015-08-30 15:43:21 -0300

    v2

--- scripts/c1_s5.py
+++ scripts/c1_s5.py
@@ -1,1 +1,20 @@
-print SOMA = 2 + 3
+'''
+Titulo: soma.py
+Funcao: Realizando uma soma com Python
+'''
+
+# Recebe o primeiro numero
+A = 2
+
+# Recebe o segundo numero
+B = 3
+
+# Realiza a soma
+SOMA = A + B
+
+# Exibe resultado na tela
+print A
+print "+"
+print B
+print "="
+print SOMA
